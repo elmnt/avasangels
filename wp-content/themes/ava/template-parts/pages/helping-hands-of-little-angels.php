47ae90632011df6fba350aa81bea73273a82a572
Greg <greg@elmnt.com>
2017-06-13 14:32:22 -0700

    Updates

--- wp-content/themes/ava/template-parts/pages/helping-hands-of-little-angels.php
+++ wp-content/themes/ava/template-parts/pages/helping-hands-of-little-angels.php
@@ -6,8 +6,42 @@
 	</div>
 
 	<div class="col-6">
-		<h4>Temporary Content</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      <?php // ACF content - The "Easy ways" title
+      if ( function_exists( 'get_field' ) ) {
+         $oaoksbtitle = get_field('oaok_sidebar_title');
+         if( $oaoksbtitle ) {
+            echo '<h4>'.$oaoksbtitle.'</h4>';
+         } else {
+            echo $GLOBALS[ 'noacf' ]; // Set in header.php
+         }
+      }
+      ?>
+
+      <?php // ACF content - The "Easy ways" list
+      if ( function_exists( 'get_field' ) ) {
+         // Check if the repeater field has rows of data
+         if( have_rows( 'oaok_sidebar_list' ) ) {
+            echo '<ol>';
+            // loop through the rows of data
+            while ( have_rows( 'oaok_sidebar_list' ) ) : the_row();
+               // set variables
+               $sidebaritem = get_sub_field( 'oaok_sidebar_list_item' );
+               echo '<li class="mb1">'.$sidebaritem.'</li>';
+            endwhile;
+            echo '</ol>';
+         } else {
+            echo $GLOBALS[ 'noacf' ]; // Set in header.php
+         }
+      }
+      ?>
+
 	</div>
+
+</div>
+
+<div class="grid--portfolio">
+
+<!-- class="gpitem" -->
 
 </div>
--- wp-content/themes/ava/template-parts/pages/our-acts-of-kindness.php
+++ wp-content/themes/ava/template-parts/pages/our-acts-of-kindness.php
@@ -1,11 +1,36 @@
 <div class="grid">
 
-	<div class="col-8">
+	<div class="col-6">
+
 		<h2>Our Acts of Kindness</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      <div class="grid">
+
+         <?php // ACF content - Acts of Kindness images/captions
+         if ( function_exists( 'get_field' ) ) {
+            // Check if the repeater field has rows of data
+            if( have_rows( 'oaok_acts' ) ) {
+               echo '<figure>';
+               // loop through the rows of data
+               while ( have_rows( 'oaok_acts' ) ) : the_row();
+                  // set variables
+                  $image = get_sub_field( 'oaok_acts_image' );
+                  $caption = get_sub_field( 'oaok_acts_caption' );
+                  echo '<img src="'.$image.'">';
+                  echo '<figcaption><p>'.$caption.'</p></figcaption>';
+               endwhile;
+               echo '</figure>';
+            } else {
+               echo $GLOBALS[ 'noacf' ]; // Set in header.php
+            }
+         }
+         ?>
+      </div>
+
 	</div>
 
-	<div class="col-4">
+	<div class="col-6">
 
       <?php // ACF content - The "Easy ways" title
       if ( function_exists( 'get_field' ) ) {
@@ -20,31 +45,19 @@
 
       <?php // ACF content - The "Easy ways" list
       if ( function_exists( 'get_field' ) ) {
-
-         /* Can't convert the strings in this case, so just use the field names.
-         $oaoksblist = get_field('oaok_sidebar_list');
-         $oaoksbitem = get_field('oaok_sidebar_list_item');
-         */
-
          // Check if the repeater field has rows of data
          if( have_rows( 'oaok_sidebar_list' ) ) {
-
             echo '<ol>';
-
             // loop through the rows of data
             while ( have_rows( 'oaok_sidebar_list' ) ) : the_row();
-               // display each sub field value
-               echo '<li>';
-               echo the_sub_field( 'oaok_sidebar_list_item' );
-               echo '</li>';
+               // set variables
+               $sidebaritem = get_sub_field( 'oaok_sidebar_list_item' );
+               echo '<li class="mb1">'.$sidebaritem.'</li>';
             endwhile;
-
             echo '</ol>';
-
          } else {
             echo $GLOBALS[ 'noacf' ]; // Set in header.php
          }
-
       }
       ?>
 
